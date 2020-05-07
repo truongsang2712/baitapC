da4a026a89e65b5af1bd889706cc70eeeea69be8
truongsang2712 <64949934+truongsang2712@users.noreply.github.com>
2020-05-07 16:08:49 +0700

    Update mangdong.c

--- mangdong.c
+++ mangdong.c
@@ -3,19 +3,19 @@
 int main()
 {
     int m, n, *ptr;
-
+    //khoi tao ma tran.
     printf("nhập số hàng của ma trận: ");
     scanf("%d", &m);
     printf("nhập số cột của ma trận: ");
     scanf("%d", &n);
-    
+    //khoi tao mang dong
     ptr = (int*)malloc((m*n) * sizeof(int));
-
+    //nhap vao ma tran tu ban phim
     for(int i=0; i<(m*n); i++) {
         printf("nhập giá trị hàng %d cột %d: ", i/n,i%n);
         scanf("%d",(ptr+i));
-
     }
+    //in du lieu ra man hinh.
     for(int i=0; i<m*n; i++) {
         printf("phần tử thứ %d là: %d \n",i, (*(ptr+i)));
     }
